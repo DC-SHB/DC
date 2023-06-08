18153997085955901eee9746f477bbe3ea9aec35
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-06-08 23:30:57 +0900

    📌 날씨 정보에 따라 날씨 UI 변경

---
+++ FashionMA/Assets/Scripts/UI/WeatherIcon.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeatherIcon : MonoBehaviour
+{
+    [SerializeField] private Sprite[] weatherIcons; // ¸¼À½, ºñ, ºñ&´«, ´«
+    [SerializeField] private Image weatherUI; // ¸ÞÀÎÈ­¸é ³¯¾¾UI
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeWeatherUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ChangeWeatherUI()
+    {
+        // °­¼ö ÇüÅÂ(PTY) : ¾øÀ½(0) / ºñ(1) / ºñ&´«(2) / ´«(3) / ¼Ò³ª±â(4) / ºø¹æ¿ï(5) / ºø¹æ¿ï´«³¯¸²(6) / ´«³¯¸²(7)
+        if (UniteData.pty == 0)
+        {
+            weatherUI.sprite = weatherIcons[0];
+        }
+
+        else if (UniteData.pty == 1 || UniteData.pty == 4 || UniteData.pty == 5)
+        {
+            weatherUI.sprite = weatherIcons[1];
+        }
+            
+        else if(UniteData.pty == 2 || UniteData.pty == 6)
+        {
+            weatherUI.sprite = weatherIcons[2];
+        }
+        else if(UniteData.pty == 3 || UniteData.pty == 7)
+        {
+            weatherUI.sprite = weatherIcons[3];
+        }
+    }
+}
