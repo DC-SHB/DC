b18e63a96b4f484e971670d06b1f387e3803e2b8
subin <ksb3458@naver.com>
2023-06-10 01:35:58 +0900

    📌 캐릭터 의상 기온과 연동

---
+++ FashionMA/Assets/Scripts/Character/CharacterClothes.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClothes : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject man1, man2, man3;
+
+    void Start()
+    {
+        man1.SetActive(false);
+        man2.SetActive(false);
+        man3.SetActive(false);
+        ChangeClothes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeClothes()
+    {
+        if (UniteData.temp >= 23.0f) man1.SetActive(true);
+        if (UniteData.temp > 17.0f && UniteData.temp < 23.0f) man2.SetActive(true);
+        if (UniteData.temp < 17.0f) man3.SetActive(true);
+    }
+}
