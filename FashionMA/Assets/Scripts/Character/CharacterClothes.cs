98751be02cd21350906e165cc58fcf03694ad142
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-06-16 19:45:55 +0900

    🛠 캐릭터 옷차림 로직 수정

--- FashionMA/Assets/Scripts/Character/CharacterClothes.cs
+++ FashionMA/Assets/Scripts/Character/CharacterClothes.cs
@@ -10,10 +10,10 @@ public class CharacterClothes : MonoBehaviour
 
     void Start()
     {
-        //man1.SetActive(false);
+        man1.SetActive(false);
         man2.SetActive(false);
         man3.SetActive(false);
-        ChangeClothes();
+
     }
 
     // Update is called once per frame
@@ -21,14 +21,13 @@ public class CharacterClothes : MonoBehaviour
     {
         if (UniteData.CharacterLoad_clothes)
         {
-            
+            ChangeClothes();
             UniteData.CharacterLoad_clothes = false;
         }
     }
 
     void ChangeClothes()
     {
-        Debug.Log("123");
         if (UniteData.temp >= 23.0f) man1.SetActive(true);
         if (UniteData.temp > 17.0f && UniteData.temp < 23.0f) man2.SetActive(true);
         if (UniteData.temp < 17.0f) man3.SetActive(true);
